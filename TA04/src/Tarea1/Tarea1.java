91f51898ed2a9a4d7dc063e4413c49c326c4115a
MarcNebot1git commit -m https://github.com/MarcNebot1/MarcNebot1.git <newgate3211@gmail.com>
2022-04-18 17:30:27 +0200

    cambios

---
+++ TA04/src/Tarea1/Tarea1.java
@@ -0,0 +1,21 @@
+package Tarea1;
+
+public class Tarea1 {
+
+	public static void main(String[] args) {
+		int a= 2;
+	    int b= 1;
+	    int suma=a+b;
+	    int resta=a-b;
+	    int multi=a*b;
+	    int division=a/b;
+	    int modulo=a%b;
+	    System.out.println("suma="+suma);
+	    System.out.println("resta="+resta);
+	    System.out.println("multi="+multi);
+	    System.out.println("división="+division);
+	    System.out.println("módulo="+modulo);
+	    
+	}
+
+}
---
+++ TA04/src/Tarea5/Tarea5.java
@@ -0,0 +1,20 @@
+package Tarea5;
+
+public class Tarea5 {
+
+	public static void main(String[] args) {
+		int A=2;
+		int B=4;
+		int C=6;
+		int D=8;
+		
+		 B=C;
+		 C=A;
+		 A=D;
+		 D=B;
+		 System.out.println(" "+B+" "+C+" "+A+" "+D);
+		
+
+	}
+
+}
